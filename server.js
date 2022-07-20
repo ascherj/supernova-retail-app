c4e75538dbd6b62747612500e72aea1a09a8658b
Jake Ascher <jake.ascher@galvanize.com>
2022-07-20 11:21:21 -0600

    Load dotenv config on server and show current IP
    This change eliminates the need to require dotenv config upon start
    of app via command line.

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static('dist'));
+require('dns').lookup(require('os').hostname(), function (err, ip, fam) {
+  app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  });
 
-app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
-);
+  app.listen(port, () =>
+    console.log(`Example app listening at ${ip}:${port}`)
+  );
+})
+
